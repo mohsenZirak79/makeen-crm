a3e365ddc0e7d351f83e9f4b8b2f849262bf4436
AliKarami23 <ali.karami.ali313@gmail.com>
2024-01-20 16:06:05 +0330

    Make a table performance_overviews

---
+++ database/migrations/2024_01_20_110028_create_performance_overviews_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::disableForeignKeyConstraints();
+        Schema::create('performance_overviews', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('course_student_id');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->integer('technical_score');
+            $table->integer('attendance_score');
+            $table->integer('reporting_score');
+            $table->integer('system_compatibility');
+            $table->string('admin_comment');
+            $table->string('mentor_comment');
+            $table->boolean('is_visible_for_user');
+            $table->timestamps();
+
+            $table->foreign('course_student_id')->references('id')->on('course_students');
+        });
+        Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('performance_overviews');
+    }
+};
