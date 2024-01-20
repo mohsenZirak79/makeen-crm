4c589989e7a11fd412498281f5008d2949789b20
AliKarami23 <ali.karami.ali313@gmail.com>
2024-01-20 16:05:28 +0330

    Make a table mentor_weekly_student_score

---
+++ database/migrations/2024_01_20_110006_create_mentor_weekly_student_score_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::disableForeignKeyConstraints();
+        Schema::create('mentor_weekly_student_score', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('course_student_id');
+            $table->integer('technical_score');
+            $table->integer('week_number');
+            $table->timestamps();
+
+            $table->foreign('course_student_id')->references('id')->on('course_students');
+        });
+        Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mentor_weekly_student_score');
+    }
+};
