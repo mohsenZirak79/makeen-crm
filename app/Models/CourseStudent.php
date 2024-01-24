82ae476b1f5849064a082a5ad2a79cac34289592
arcan5331 <alighaforian@yahoo.com>
2024-01-24 17:34:45 +0330

    made CourseStudent a pivot model.

--- app/Models/CourseStudent.php
+++ app/Models/CourseStudent.php
@@ -4,11 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\Pivot;
 
-class CourseStudent extends Model
+class CourseStudent extends Pivot
 {
     use HasFactory;
+
+    public $incrementing = true;
     protected $table = 'course_students';
+    protected $foreignKey = 'course_id';
+    protected $relatedKey = 'student_id';
 
     protected $fillable = [
         'student_id',
@@ -18,24 +26,27 @@ class CourseStudent extends Model
         'installment_data_id',
     ];
 
-    public function student()
+    public function student(): BelongsTo
     {
         return $this->belongsTo(User::class, 'student_id');
     }
 
-    public function course()
+    public function course(): BelongsTo
     {
         return $this->belongsTo(Course::class, 'course_id');
     }
-    public function courseInstallment()
+
+    public function courseInstallment(): HasOne
     {
         return $this->hasOne(CourseInstallment::class, 'installment_data_id');
     }
-    public function leaveRequests()
+
+    public function leaveRequests(): HasMany
     {
         return $this->hasMany(LeaveRequest::class, 'course_student_id');
     }
-    public function reports()
+
+    public function reports(): HasMany
     {
         return $this->hasMany(Report::class, 'course_student_id');
     }
