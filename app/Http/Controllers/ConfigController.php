4bec4957528e42585434b6dbf401c910b0f6e291
Mohammadreza Abdoli <sirmohammadra@gmail.com>
2024-01-27 10:30:09 +0330

    Created request for config update and wrote function for that.

--- app/Http/Controllers/ConfigController.php
+++ app/Http/Controllers/ConfigController.php
@@ -2,12 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Config\UpdateConfigRequest;
 use App\Http\Resources\Config\ConfigResource;
+use App\Models\Config;
 
 class ConfigController extends Controller
 {
     public function index()
     {
         return new ConfigResource();
+    }
+
+    public function update(UpdateConfigRequest $request)
+    {
+        $validated = $request->all();
+        foreach ($validated as $key => $value) {
+            Config::where('key', $key)->update(['value' => $value]);
+        }
+        return response()->json([
+            'message' => 'Config updated successfully'
+        ]);
     }
 }
