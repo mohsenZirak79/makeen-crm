404eeb4d77aa6eb29105e82e704c617dc4bfb7c2
AliKarami23 <ali.karami.ali313@gmail.com>
2024-01-27 22:38:42 +0330

    Create authcontroller and write login, logout

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $user = User::where('national_id', $request->national_id)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The username or password is incorrect.']
+            ]);
+        }
+
+        $token = $user->createToken('UserToken')->plainTextToken;
+
+        return response()->json([
+            'token' => $token,
+            'user' => $user
+        ]);
+    }
+
+    public function Logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+        return response()->json([
+            'logout' => 'TOKEN Removed'
+        ]);
+    }
+}
