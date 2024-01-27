29634e4aaf968f7ce7953ea02f1bfe215fbdaba7
Mohammadreza Abdoli <sirmohammadra@gmail.com>
2024-01-27 09:16:24 +0330

    Created resource for config and added index function.

--- app/Http/Controllers/ConfigController.php
+++ app/Http/Controllers/ConfigController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Resources\Config\ConfigResource;
 
 class ConfigController extends Controller
 {
-    
+    public function index()
+    {
+        return new ConfigResource();
+    }
 }
