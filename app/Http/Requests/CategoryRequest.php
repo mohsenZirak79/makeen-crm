c5d97b46742807f23444be57f390267500bb5e6c
meysamansari <meysamansari2023@gmail.com>
2024-02-03 01:08:08 +0330

    add CategoryRequest rules

--- app/Http/Requests/CategoryRequest.php
+++ app/Http/Requests/CategoryRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class CategoryRequest extends FormRequest
 {
@@ -11,7 +12,7 @@ class CategoryRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +23,14 @@ class CategoryRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|string|max:255',
+            'topic_id' => [
+                'nullable',
+                'integer',
+                Rule::exists('categories', 'id')->where(function ($query) {
+                    $query->whereNull('topic_id');
+                }),
+            ],
         ];
     }
 }
