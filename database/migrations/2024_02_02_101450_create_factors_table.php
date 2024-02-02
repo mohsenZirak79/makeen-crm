3c1303959cb6260f3daae293be82aae7a146d05c
arcan5331 <alighaforian@yahoo.com>
2024-02-02 13:55:04 +0330

    added factors table migration.

---
+++ database/migrations/2024_02_02_101450_create_factors_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('factors', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('course_installments_id')->nullable()->constrained('course_installments');
+            $table->foreignId('user_id')->nullable()->constrained('users');
+            $table->unsignedBigInteger('total_amount');
+            $table->unsignedBigInteger('amount_paid')->default(0);
+            $table->enum('status', ['pending', 'paid', 'overdue'])->default('pending');
+            $table->date('du_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('factors', function (Blueprint $table) {
+            $table->dropForeign('factors_course_installments_id_foreign');
+            $table->dropForeign('factors_user_id_foreign');
+        });
+        Schema::dropIfExists('factors');
+    }
+};
