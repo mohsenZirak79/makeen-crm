bc5060bcdd6d4db64c182b4999d53985ffdb736e
arcan5331 <alighaforian@yahoo.com>
2024-02-03 21:43:26 +0330

    added CourseInstallment HasManyThrough relation to Course.

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 
 class Course extends Model
 {
@@ -36,6 +37,11 @@ class Course extends Model
     public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class, 'sub_category_id');
+    }
+
+    public function courseInstallments(): HasManyThrough
+    {
+        return $this->hasManyThrough(CourseInstallment::class, CourseStudent::class, 'course_id', 'course_student_id');
     }
 
     public function students(): BelongsToMany
