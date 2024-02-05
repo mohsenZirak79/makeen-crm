ea4eb4c9378aac15622c267f5e2bd5383044b524
arcan5331 <alighaforian@yahoo.com>
2024-02-05 12:50:43 +0330

    added FactorController and factor route.

---
+++ app/Http/Controllers/FactorController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Factor;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class FactorController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function show(Factor $factor)
+    {
+
+    }
+
+    public function create(User $user)
+    {
+
+    }
+
+    public function update(Factor $factor)
+    {
+
+    }
+
+    public function delete(Factor $factor)
+    {
+
+    }
+}
---
+++ routes/factor/factor.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\FactorController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+Route::group([
+    'as' => 'factor.',
+    'prefix' => 'prefix',
+    'middleware' => ['auth:sanctum']
+], function () {
+    Route::get('/', [FactorController::class, 'index'])->name('index');
+    Route::get('/{factor}', [FactorController::class, 'show'])->name('show');
+    Route::post('/{user}', [FactorController::class, 'create'])->name('create');
+    Route::put('/{factor}', [FactorController::class, 'update'])->name('update');
+    Route::delete('/{factor}', [FactorController::class, 'delete'])->name('delete');
+});
