af2c567d171fdb698cd3b54658f04d59db34f4ae
Mohammadreza Abdoli <sirmohammadra@gmail.com>
2024-02-05 13:34:42 +0330

    Added media library trait.

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -5,10 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
 class Transaction extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes, InteractsWithMedia;
+
     protected $table = 'transactions';
 
     protected $fillable = [
--- app/Models/User.php
+++ app/Models/User.php
@@ -14,11 +14,12 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
 class User extends Authenticatable
 {
 
-    use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
+    use HasApiTokens, HasFactory, Notifiable, SoftDeletes, InteractsWithMedia;
 
     protected $fillable = [
         'first_name',
