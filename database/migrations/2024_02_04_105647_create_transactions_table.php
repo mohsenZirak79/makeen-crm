c96d9aee5ebe2969309a0d426603ee3dbdd439fa
arcan5331 <alighaforian@yahoo.com>
2024-02-05 11:43:49 +0330

    added transactions table migration.

---
+++ database/migrations/2024_02_04_105647_create_transactions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('factors_id');
+            $table->foreign('factors_id')->references('id')->on('factors');
+            $table->bigInteger('amount');
+            $table->set('status', ['pending', 'paid', 'failed'])->default('pending');
+            $table->json('details')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
