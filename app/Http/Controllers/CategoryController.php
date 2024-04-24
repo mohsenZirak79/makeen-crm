aec88c82feae8b1bac93b4d6b52525feccabac6a
meysamansari <143381707+meysamansari@users.noreply.github.com>
2024-04-24 17:21:28 +0330

    add crud for category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,62 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CategoryRequest;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    public function getCategories(Request $request, $type)
+    {
+        switch ($type) {
+            case 'subcategory':
+                $categories = Category::whereNotNull('topic_id')->get();
+                break;
+            case 'main':
+                $categories = Category::whereNull('topic_id')->get();
+                break;
+            default:
+                $categories = Category::all();
+                break;
+        }
+
+        return response()->json([
+            'categories' => $categories
+        ]);
+    }
+    public function store(CategoryRequest $request)
+    {
+        $category = Category::create($request->all());
+        return response()->json([
+            'message' => 'category created successfully.',
+            'category' => $category]);
+    }
+    public function show(Category $category)
+    {
+        return response()->json([
+            'category' => $category]);
+    }
+    public function update(CategoryRequest $request, Category $category)
+    {
+        $category->update($request->all());
+        return response()->json([
+            'message' => 'category updated successfully.',
+            'category' => $category]);
+    }
+    public function destroy(Category $category)
+    {
+        $category->delete();
+        return response()->json([
+            'message' => 'category deleted successfully.',
+            'category' => $category],204);
+    }
+
+    public function getSubcategories(Category $category)
+    {
+        $subcategories = Category::where('topic_id', $category->id)->get();
+        return response()->json([
+            'subcategories' => $subcategories
+        ]);
+    }
 }
--- routes/category/category.php
+++ routes/category/category.php
@@ -1,1 +1,14 @@
 <?php
+
+use App\Http\Controllers\CategoryController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('category')->middleware('auth:sanctum')->group(function () {
+    Route::get('/{type}', [CategoryController::class, 'getCategories'])
+        ->where('type', 'subcategory|main|all');
+    Route::post('/', [CategoryController::class, 'store']);
+    Route::get('/{category}', [CategoryController::class, 'show']);
+    Route::put('/{category}', [CategoryController::class, 'update']);
+    Route::delete('/{category}', [CategoryController::class, 'destroy']);
+    Route::get('/{category}/subcategories', [CategoryController::class, 'getSubcategories']);
+});
